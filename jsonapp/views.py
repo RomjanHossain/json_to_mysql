e97b1a22ae2f8a43a788904812308116a294e1b8
RomjanHossain <romjanhossain726526@gmail.com>
2022-11-30 11:03:57 +0600

    method upgrade

--- jsonapp/views.py
+++ jsonapp/views.py
@@ -4,6 +4,7 @@ from jsontomysql.settings import BASE_DIR
 from .models import JsonModel
 
 # Create your views here.
+# mysql -u username -p -h localhost DATA-BASE-NAME < data.sql
 
 
 def index(request):
@@ -14,27 +15,31 @@ def index(request):
     #     for i in range(114)
     #     if i != 1
     # ]
-    # for i in json_datas:
-    #     for data in range(len(i)):
-    #         # JsonModel.objects.create(**i[data])
-    #         # data = json.load(json_data)
-    #         q0 = JsonModel(
-    #             id=i[0]["id"],
-    #             verse_number=i[data]["verse_number"],
-    #             verse_key=i[data]["verse_key"],
-    #             hizb_number=i[data]["hizb_number"],
-    #             rub_el_hizb_number=i[data]["rub_el_hizb_number"],
-    #             ruku_number=i[data]["ruku_number"],
-    #             manzil_number=i[data]["manzil_number"],
-    #             sajdah_number=i[data]["sajdah_number"],
-    #             text_uthmani=i[data]["text_uthmani"],
-    #             chapter_id=i[data]["chapter_id"],
-    #             text_imlaei_simple=i[data]["text_imlaei_simple"],
-    #             juz_number=i[data]["juz_number"],
-    #             translations=i[data]["translations"],
-    #         )
-    #         print("now doing -> ", q0.chapter_id, q0.verse_number)
-    #         q0.save()
+    for i in range(114):
+        if i != 1:
+            json_data = open(os.path.join(BASE_DIR, "static", f"json/quran_{i}.json"))
+            data = json.load(json_data)
+            for j in data:
+                # for data in range(len(i)):
+                #     # JsonModel.objects.create(**i[data])
+                #     # data = json.load(json_data)
+                q0 = JsonModel(
+                    id=j["id"],
+                    verse_number=j["verse_number"],
+                    verse_key=j["verse_key"],
+                    hizb_number=j["hizb_number"],
+                    rub_el_hizb_number=j["rub_el_hizb_number"],
+                    ruku_number=j["ruku_number"],
+                    manzil_number=j["manzil_number"],
+                    sajdah_number=j["sajdah_number"],
+                    text_uthmani=j["text_uthmani"],
+                    chapter_id=j["chapter_id"],
+                    text_imlaei_simple=j["text_imlaei_simple"],
+                    juz_number=j["juz_number"],
+                    translations=j["translations"],
+                )
+                print("now doing -> ", q0.chapter_id, q0.verse_number)
+                q0.save()
 
     # q0.save()
     return render(
