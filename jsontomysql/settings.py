3b79dde896efea6aa9b8d8f3c8c508d645b2f622
RomjanHossain <romjanhossain726526@gmail.com>
2022-11-30 11:03:27 +0600

    env setting

--- jsontomysql/settings.py
+++ jsontomysql/settings.py
@@ -13,11 +13,12 @@ import os
 from pathlib import Path
 import environ
 
-env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
